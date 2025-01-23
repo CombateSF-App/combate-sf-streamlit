1e40fbf88cace20498741e3989702d2fb6906e48
CombateSF-App <combatesf.app@gmail.com>
2025-01-23 08:31:22 -0500

    Update New_Home.py

--- app_final/New_Home.py
+++ app_final/New_Home.py
@@ -9,15 +9,6 @@ import contextily as ctx
 import locale
 import io
 from PyPDF2 import PdfWriter, PdfReader
-import pathlib
-import sys
-import os
-
-print(os.listdir(os.getcwd()))
-print('0000000000000000000000000000000')
-
-dir = pathlib.Path(__file__).absolute()
-sys.path.append(dir)
 
 # Definindo a língua (utilizado para extrair o mês do formato datetime em português)
 #locale.setlocale(locale.LC_TIME, 'Portuguese_Brazil.1252')
@@ -29,7 +20,7 @@ st.set_page_config(page_title="MAXSATT - Plataforma de Monitoramento", layout="w
 #st.markdown("<h1 style='text-align:center;'font-size:40px;'>Plataforma de Monitoramento de Formigas por Sensoriamento Remoto</h1>", unsafe_allow_html=True)
 
 # Adicionando a logo do Maxsatt na aba lateral
-st.sidebar.image("./logos/logotipo_Maxsatt.png", use_container_width=False, width=150)
+st.sidebar.image("app-final/logos/logotipo_Maxsatt.png", use_container_width=False, width=150)
 
 # Importando bases de dados (alterar aqui para mudar a base referenciada)
 pred_attack = pd.read_parquet("prediction/Filtered_pred_attack.parquet")
