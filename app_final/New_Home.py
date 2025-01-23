dab0eda553e7c55a66efe82fff5383a33e0ef2bc
CombateSF-App <combatesf.app@gmail.com>
2025-01-23 09:48:00 -0300

    Update New_Home.py

--- app_final/New_Home.py
+++ app_final/New_Home.py
@@ -20,11 +20,11 @@ st.set_page_config(page_title="MAXSATT - Plataforma de Monitoramento", layout="w
 #st.markdown("<h1 style='text-align:center;'font-size:40px;'>Plataforma de Monitoramento de Formigas por Sensoriamento Remoto</h1>", unsafe_allow_html=True)
 
 # Adicionando a logo do Maxsatt na aba lateral
-st.sidebar.image("logos\logotipo_Maxsatt.png", use_column_width=False, width=150)
+st.sidebar.image("logos/logotipo_Maxsatt.png", use_column_width=False, width=150)
 
 # Importando bases de dados (alterar aqui para mudar a base referenciada)
-pred_attack = pd.read_parquet("prediction\Filtered_pred_attack.parquet")
-stands_all = gpd.read_file("prediction\Talhoes_Manulife_2.shp")
+pred_attack = pd.read_parquet("prediction/Filtered_pred_attack.parquet")
+stands_all = gpd.read_file("prediction/Talhoes_Manulife_2.shp")
 
 # Tratando a base pred_attack
 pred_attack['COMPANY'] = pred_attack['COMPANY'].str.upper()
